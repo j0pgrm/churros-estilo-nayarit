8f0a3e6f08a83d73e4467156a698fce71cc31b70
name <your mail>
2025-06-27 17:44:29 -0700

    location change

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -1,8 +1,6 @@
-import React, { useMemo } from 'react'
 import Nav from './Components/Nav';
 import Footer from './Components/Footer'; 
 import '../styles/pages/locations.scss'; 
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api"; 
 import churroTruck from '../img/churro-truck.jpg'; 
 import churroStandModel from '../img/churro-stand-model.jpg'; 
 import churroStand from '../img/churro-stand.jpg'; 
@@ -10,13 +8,6 @@ import churroLady from '../img/churro-lady.jpg';
 import churrosMenu from '../img/churros-menu-pic.jpeg'; 
 
 function Locations() {
-
-  const { isLoaded } = useLoadScript({
-    // googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
-    googleMapsApiKey: "AIzaSyCArtdL8CKJGnW67Mc9rrFPkq2dSyhQRm4",
-  });
-
-  const center = useMemo(() => ({ lat: 33.478490, lng: -112.177580 }), []);
 
   return (
     <div className="locations-page">
@@ -31,21 +22,7 @@ function Locations() {
           <p>2813 N 55th Ave Phoenix, AZ</p>
 
           <div className="location-box-container">
-            {/* {
-              !isLoaded ? (
-                
-                <h1>Loading...</h1>
 
-              ) : (
-
-                <GoogleMap
-                  mapContainerClassName="map-container"
-                  center={center}
-                  zoom={17}
-                >
-                  <Marker position={{ lat: 33.478490, lng: -112.177580 }} />
-                </GoogleMap>
-            )} */}
           </div>
 
           <div className="hours-container">
